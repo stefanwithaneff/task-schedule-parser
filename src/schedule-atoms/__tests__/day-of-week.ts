7ccb568ff35b9211052dd5b7a0005fe4c18c922d
Stefan <>
2020-08-07 09:29:34 -0500

    Implement Day of Week scheduling atoms and add tests for them

---
+++ src/schedule-atoms/__tests__/day-of-week.ts
@@ -0,0 +1,242 @@
+import { DateTime } from "luxon";
+import {
+  DayOfWeekValueAtom,
+  DayOfWeekRangeAtom,
+  LastDayOfWeekAtom,
+  NthDayOfWeekAtom,
+} from "../day-of-week";
+import { DayOfMonthRangeAtom } from "../day-of-month";
+import { InvalidRangeError } from "../errors";
+
+describe("Day of Week Scheduling Atoms", () => {
+  describe("Day of Week Value atom", () => {
+    it("throws a RangeError when the provided value is less than the minimum possible value", () => {
+      expect(() => new DayOfWeekValueAtom(-1)).toThrow(RangeError);
+    });
+
+    it("throws a RangeError when the provided value is greater than the maximum possible value", () => {
+      expect(() => new DayOfWeekValueAtom(7)).toThrow(RangeError);
+    });
+
+    it("returns the next day of the month that matches the given weekday", () => {
+      const atom = new DayOfWeekValueAtom(3);
+      const date1 = DateTime.local(2020, 8, 3);
+      const date2 = DateTime.local(2020, 8, 5);
+      const date3 = DateTime.local(2020, 8, 7);
+
+      expect(atom.getNextDateAfter(date1)).toEqual(date1.set({ day: 5 }));
+      expect(atom.getNextDateAfter(date2)).toEqual(date2.set({ day: 12 }));
+      expect(atom.getNextDateAfter(date3)).toEqual(date3.set({ day: 12 }));
+    });
+
+    it("correctly returns the next Sunday of the month if 0 is provided", () => {
+      const atom = new DayOfWeekValueAtom(0);
+      const date1 = DateTime.local(2020, 8, 3);
+      const date2 = DateTime.local(2020, 8, 9);
+
+      expect(atom.getNextDateAfter(date1)).toEqual(date1.set({ day: 9 }));
+      expect(atom.getNextDateAfter(date2)).toEqual(date2.set({ day: 16 }));
+    });
+
+    it("returns null if the next matching day occurs in the next month", () => {
+      const atom = new DayOfWeekValueAtom(3);
+      const date = DateTime.local(2020, 8, 27);
+
+      expect(atom.getNextDateAfter(date)).toBe(null);
+    });
+
+    it("returns true if the provided date matches the given weekday", () => {
+      const atom1 = new DayOfWeekValueAtom(0);
+      const date1 = DateTime.local(2020, 8, 9);
+      const atom2 = new DayOfWeekValueAtom(3);
+      const date2 = DateTime.local(2020, 8, 5);
+
+      expect(atom1.isValid(date1)).toBe(true);
+      expect(atom2.isValid(date2)).toBe(true);
+    });
+
+    it("returns false if the provided date matches the given weekday", () => {
+      const atom1 = new DayOfWeekValueAtom(0);
+      const date1 = DateTime.local(2020, 8, 5);
+      const atom2 = new DayOfWeekValueAtom(3);
+      const date2 = DateTime.local(2020, 8, 9);
+
+      expect(atom1.isValid(date1)).toBe(false);
+      expect(atom2.isValid(date2)).toBe(false);
+    });
+  });
+
+  describe("Day of Week Range atom", () => {
+    it("throws a RangeError when the provided value is less than the minimum possible value", () => {
+      expect(() => new DayOfWeekRangeAtom(-1, 4)).toThrow(RangeError);
+    });
+
+    it("throws a RangeError when the provided value is greater than the maximum possible value", () => {
+      expect(() => new DayOfWeekRangeAtom(2, 7)).toThrow(RangeError);
+    });
+
+    it("throws an InvalidRangeError when the provided min is greater than the provided max", () => {
+      expect(() => new DayOfMonthRangeAtom(4, 2)).toThrow(InvalidRangeError);
+    });
+
+    it("returns the next date matching the range of weekdays", () => {
+      const atom = new DayOfWeekRangeAtom(1, 4);
+      const date1 = DateTime.local(2020, 8, 6);
+      const date2 = DateTime.local(2020, 8, 9);
+      const date3 = DateTime.local(2020, 8, 11);
+
+      expect(atom.getNextDateAfter(date1)).toEqual(date1.set({ day: 10 }));
+      expect(atom.getNextDateAfter(date2)).toEqual(date2.set({ day: 10 }));
+      expect(atom.getNextDateAfter(date3)).toEqual(date3.set({ day: 12 }));
+    });
+
+    it("returns null if the next matching date in the range occurs in the next month", () => {
+      const atom = new DayOfWeekRangeAtom(1, 4);
+      const date = DateTime.local(2020, 8, 31);
+
+      expect(atom.getNextDateAfter(date)).toBe(null);
+    });
+
+    it("returns true if the date matches the provided range of weekdays", () => {
+      const atom1 = new DayOfWeekRangeAtom(3, 6);
+      const date1 = DateTime.local(2020, 8, 6);
+      const atom2 = new DayOfWeekRangeAtom(0, 2);
+      const date2 = DateTime.local(2020, 8, 9);
+
+      expect(atom1.isValid(date1)).toBe(true);
+      expect(atom2.isValid(date2)).toBe(true);
+    });
+
+    it("returns false if the date does not match the provided range of weekdays", () => {
+      const atom1 = new DayOfWeekRangeAtom(3, 6);
+      const date1 = DateTime.local(2020, 8, 9);
+      const atom2 = new DayOfWeekRangeAtom(0, 2);
+      const date2 = DateTime.local(2020, 8, 6);
+
+      expect(atom1.isValid(date1)).toBe(false);
+      expect(atom2.isValid(date2)).toBe(false);
+    });
+  });
+
+  describe("Last Day of Week atom", () => {
+    it("throws a RangeError when the provided value is less than the minimum possible value", () => {
+      expect(() => new LastDayOfWeekAtom(-1)).toThrow(RangeError);
+    });
+
+    it("throws a RangeError when the provided value is greater than the maximum possible value", () => {
+      expect(() => new LastDayOfWeekAtom(7)).toThrow(RangeError);
+    });
+
+    it("returns the last day of the month if it matches the provided weekday", () => {
+      const atom = new LastDayOfWeekAtom(1);
+      const date = DateTime.local(2020, 8, 15);
+
+      expect(atom.getNextDateAfter(date)).toEqual(date.set({ day: 31 }));
+    });
+
+    it("returns the last nth day of the month that matches the provided value", () => {
+      const atom1 = new LastDayOfWeekAtom(0);
+      const atom2 = new LastDayOfWeekAtom(4);
+      const date = DateTime.local(2020, 8, 15);
+
+      expect(atom1.getNextDateAfter(date)).toEqual(date.set({ day: 30 }));
+      expect(atom2.getNextDateAfter(date)).toEqual(date.set({ day: 27 }));
+    });
+
+    it("returns null if the provided date occurs on or after the last nth day of the month", () => {
+      const atom = new LastDayOfWeekAtom(4);
+      const date1 = DateTime.local(2020, 8, 27);
+      const date2 = DateTime.local(2020, 8, 29);
+
+      expect(atom.getNextDateAfter(date1)).toBe(null);
+      expect(atom.getNextDateAfter(date2)).toBe(null);
+    });
+
+    it("returns true if the provided date is the last matching weekday in the month", () => {
+      const atom1 = new LastDayOfWeekAtom(4);
+      const date1 = DateTime.local(2020, 8, 27);
+      const atom2 = new LastDayOfWeekAtom(0);
+      const date2 = DateTime.local(2020, 8, 30);
+
+      expect(atom1.isValid(date1)).toBe(true);
+      expect(atom2.isValid(date2)).toBe(true);
+    });
+
+    it("returns false if the provided date is not the last matching weekday in the month", () => {
+      const atom1 = new LastDayOfWeekAtom(4);
+      const date1 = DateTime.local(2020, 8, 29);
+      const atom2 = new LastDayOfWeekAtom(0);
+      const date2 = DateTime.local(2020, 8, 16);
+
+      expect(atom1.isValid(date1)).toBe(false);
+      expect(atom2.isValid(date2)).toBe(false);
+    });
+  });
+
+  describe("Nth Day of Week atom", () => {
+    it("throws a RangeError if the provided day of week is outside of the valid range", () => {
+      expect(() => new NthDayOfWeekAtom(new DayOfWeekValueAtom(-1), 3)).toThrow(
+        RangeError
+      );
+      expect(() => new NthDayOfWeekAtom(new DayOfWeekValueAtom(7), 3)).toThrow(
+        RangeError
+      );
+    });
+
+    it("throws a RangeError if the provided ordinal is outside of the valid range", () => {
+      expect(() => new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 0)).toThrow(
+        RangeError
+      );
+      expect(() => new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 6)).toThrow(
+        RangeError
+      );
+    });
+
+    it("returns the nth occurrence of the weekday in the provided date's month", () => {
+      const atom1 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(5), 4);
+      const atom2 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(0), 2);
+      const atom3 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 3);
+      const atom4 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(1), 5);
+      const date = DateTime.local(2020, 8, 4);
+
+      expect(atom1.getNextDateAfter(date)).toEqual(date.set({ day: 28 }));
+      expect(atom2.getNextDateAfter(date)).toEqual(date.set({ day: 9 }));
+      expect(atom3.getNextDateAfter(date)).toEqual(date.set({ day: 18 }));
+      expect(atom4.getNextDateAfter(date)).toEqual(date.set({ day: 31 }));
+    });
+
+    it("returns null if the provided date is after the nth weekday of that month", () => {
+      const atom = new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 3);
+      const date = DateTime.local(2020, 8, 20);
+
+      expect(atom.getNextDateAfter(date)).toBe(null);
+    });
+
+    it("returns null if the nth weekday does not exist in that month", () => {
+      const atom = new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 5);
+      const date = DateTime.local(2020, 8, 1);
+
+      expect(atom.getNextDateAfter(date)).toBe(null);
+    });
+
+    it("returns true if the provided date is the nth weekday of the month", () => {
+      const atom1 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 3);
+      const date1 = DateTime.local(2020, 8, 18);
+      const atom2 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(0), 5);
+      const date2 = DateTime.local(2020, 8, 30);
+
+      expect(atom1.isValid(date1)).toBe(true);
+      expect(atom2.isValid(date2)).toBe(true);
+    });
+
+    it("returns false if the provided date is not the nth weekday of the month", () => {
+      const atom1 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(2), 3);
+      const date1 = DateTime.local(2020, 8, 25);
+      const atom2 = new NthDayOfWeekAtom(new DayOfWeekValueAtom(0), 5);
+      const date2 = DateTime.local(2020, 8, 29);
+
+      expect(atom1.isValid(date1)).toBe(false);
+      expect(atom2.isValid(date2)).toBe(false);
+    });
+  });
+});
---
+++ src/schedule-atoms/day-of-week.ts
@@ -0,0 +1,211 @@
+import { DateTime } from "luxon";
+import { ScheduleAtom } from "./base";
+import { InvalidRangeError } from "./errors";
+
+const MIN_CRON_DAY_OF_WEEK_VALUE = 0;
+const MAX_CRON_DAY_OF_WEEK_VALUE = 6;
+
+// Cron values are from 0-6/SUN-SAT, Luxon ranges from 1-7/MON-SUN
+function convertLuxonValueToCronValue(luxonValue: number): number {
+  return luxonValue === 7 ? 0 : luxonValue;
+}
+
+export class DayOfWeekValueAtom implements ScheduleAtom {
+  readonly value: number;
+  constructor(value: number) {
+    if (
+      value < MIN_CRON_DAY_OF_WEEK_VALUE ||
+      value > MAX_CRON_DAY_OF_WEEK_VALUE
+    ) {
+      throw new RangeError(
+        `Invalid value for Day of Week: ${value}. Must be between ${MIN_CRON_DAY_OF_WEEK_VALUE} and ${MAX_CRON_DAY_OF_WEEK_VALUE}`
+      );
+    }
+    this.value = value;
+  }
+
+  getNextDateAfter(date: DateTime) {
+    const currentWeekday = convertLuxonValueToCronValue(date.weekday);
+
+    let nextDate;
+    if (currentWeekday < this.value) {
+      nextDate = date.plus({ days: this.value - currentWeekday });
+    } else if (currentWeekday === this.value) {
+      nextDate = date.plus({ days: 7 });
+    } else {
+      nextDate = date.plus({ days: 7 - currentWeekday + this.value });
+    }
+
+    if (nextDate.month === date.month) {
+      return nextDate.startOf("day");
+    }
+
+    return null;
+  }
+
+  isValid(date: DateTime) {
+    return convertLuxonValueToCronValue(date.weekday) === this.value;
+  }
+
+  toString() {
+    return `Value(${this.value})`;
+  }
+}
+
+export class DayOfWeekRangeAtom implements ScheduleAtom {
+  readonly min: number;
+  readonly max: number;
+
+  constructor(
+    minAtom: DayOfWeekValueAtom | number,
+    maxAtom: DayOfWeekValueAtom | number
+  ) {
+    const min = minAtom instanceof DayOfWeekValueAtom ? minAtom.value : minAtom;
+    const max = maxAtom instanceof DayOfWeekValueAtom ? maxAtom.value : maxAtom;
+    if (min < MIN_CRON_DAY_OF_WEEK_VALUE) {
+      throw new RangeError(
+        `Invalid minimum value for Day of Week Range: ${min}. Must be between ${MIN_CRON_DAY_OF_WEEK_VALUE} and ${MAX_CRON_DAY_OF_WEEK_VALUE}`
+      );
+    }
+
+    if (max > MAX_CRON_DAY_OF_WEEK_VALUE) {
+      throw new RangeError(
+        `Invalid maximum value for Day of Week Range: ${max}. Must be between ${MIN_CRON_DAY_OF_WEEK_VALUE} and ${MAX_CRON_DAY_OF_WEEK_VALUE}`
+      );
+    }
+
+    if (min > max) {
+      throw new InvalidRangeError(
+        `Min range value cannot be higher than the max value for Day of Week. Min: ${min}, Max: ${max}`
+      );
+    }
+
+    this.min = min;
+    this.max = max;
+  }
+
+  getNextDateAfter(date: DateTime) {
+    const currentWeekday = convertLuxonValueToCronValue(date.weekday);
+
+    let nextDate: DateTime;
+    if (currentWeekday < this.min) {
+      nextDate = date.plus({ days: this.min - currentWeekday });
+    } else if (currentWeekday < this.max) {
+      nextDate = date.plus({ days: 1 });
+    } else {
+      nextDate = date.plus({ days: 7 - currentWeekday + this.min });
+    }
+
+    if (nextDate.month === date.month) {
+      return nextDate.startOf("day");
+    }
+    return null;
+  }
+
+  isValid(date: DateTime) {
+    const currentWeekday = convertLuxonValueToCronValue(date.weekday);
+
+    return currentWeekday >= this.min && currentWeekday <= this.max;
+  }
+}
+
+export class LastDayOfWeekAtom implements ScheduleAtom {
+  readonly value: number;
+
+  constructor(value: number) {
+    if (
+      value < MIN_CRON_DAY_OF_WEEK_VALUE ||
+      value > MAX_CRON_DAY_OF_WEEK_VALUE
+    ) {
+      throw new RangeError(
+        `Invalid value for Day of Week: ${value}L. Must be between ${MIN_CRON_DAY_OF_WEEK_VALUE} and ${MAX_CRON_DAY_OF_WEEK_VALUE}`
+      );
+    }
+    this.value = value;
+  }
+
+  getNextDateAfter(date: DateTime) {
+    const lastDay = date.set({ day: date.daysInMonth });
+    const lastWeekdayOfMonth = convertLuxonValueToCronValue(lastDay.weekday);
+
+    let nextDate: DateTime;
+    if (lastWeekdayOfMonth === this.value) {
+      nextDate = lastDay;
+    } else if (lastWeekdayOfMonth > this.value) {
+      nextDate = lastDay.minus({ day: lastWeekdayOfMonth - this.value });
+    } else {
+      nextDate = lastDay.minus({ day: 7 - this.value + lastWeekdayOfMonth });
+    }
+
+    if (date.day < nextDate.day) {
+      return nextDate.startOf("day");
+    }
+
+    return null;
+  }
+
+  isValid(date: DateTime) {
+    return (
+      convertLuxonValueToCronValue(date.weekday) === this.value &&
+      date.daysInMonth - date.day < 7
+    );
+  }
+
+  toString() {
+    return `LastDayOfWeek(${this.value})`;
+  }
+}
+
+export class NthDayOfWeekAtom implements ScheduleAtom {
+  readonly dayOfWeek: number;
+  readonly ordinal: number;
+
+  constructor(dayOfWeek: DayOfWeekValueAtom, ordinal: number) {
+    if (
+      dayOfWeek.value < MIN_CRON_DAY_OF_WEEK_VALUE ||
+      dayOfWeek.value > MAX_CRON_DAY_OF_WEEK_VALUE
+    ) {
+      throw new RangeError(
+        `Invalid value for Day of Week: ${dayOfWeek.value}#${ordinal}. Must be between ${MIN_CRON_DAY_OF_WEEK_VALUE} and ${MAX_CRON_DAY_OF_WEEK_VALUE}`
+      );
+    }
+
+    if (ordinal < 1 || ordinal > 5) {
+      throw new RangeError(
+        `Invalid ordinal for Day of Week: ${dayOfWeek.value}#${ordinal}. Must be between 1 and 5`
+      );
+    }
+
+    this.dayOfWeek = dayOfWeek.value;
+    this.ordinal = ordinal;
+  }
+
+  getNextDateAfter(date: DateTime) {
+    const startOfMonth = date.startOf("month");
+    const startingWeekday = convertLuxonValueToCronValue(startOfMonth.weekday);
+
+    let weekdayOffset: number;
+    if (startingWeekday <= this.dayOfWeek) {
+      weekdayOffset = this.dayOfWeek - startingWeekday;
+    } else {
+      weekdayOffset = 7 - startingWeekday + this.dayOfWeek;
+    }
+
+    const weekOffset = this.ordinal - 1;
+    const totalOffset = weekdayOffset + weekOffset * 7;
+    const nextDate = startOfMonth.plus({ days: totalOffset });
+
+    if (date.month === nextDate.month && date.day < nextDate.day) {
+      return nextDate.startOf("day");
+    }
+
+    return null;
+  }
+
+  isValid(date: DateTime) {
+    return (
+      convertLuxonValueToCronValue(date.weekday) === this.dayOfWeek &&
+      Math.ceil(date.day / 7) === this.ordinal
+    );
+  }
+}
